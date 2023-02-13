493b440c16f8b2c5bafa89e46947130f51215bc5
James Eastham <jamesuk@amazon.com>
2023-02-13 12:20:26 +0000

    Update SSR example

--- src/axum-lambda-server-side-rendering/src/main.rs
+++ src/axum-lambda-server-side-rendering/src/main.rs
@@ -1,8 +1,11 @@
+mod services;
+
 use std::{env, io::Write};
 
 use aws_config::SdkConfig;
 use aws_sdk_dynamodb::{model::AttributeValue, Client};
 use axum::http::StatusCode;
+use axum::response::Redirect;
 use axum::{
     error_handling::HandleErrorLayer,
     extract::Path,
@@ -12,8 +15,8 @@ use axum::{
     Form, Router,
 };
 use lambda_http::{aws_lambda_events::serde::Deserialize, run, Error};
-use serde::Serialize;
 use serde_json::{json, Value};
+use services::services::{Todo, CreateTodo, TodoService};
 use std::{net::SocketAddr, sync::Arc, time::Duration};
 use tower::{BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
@@ -24,8 +27,7 @@ use uuid::Uuid;
 mod axum_ructe;
 
 struct AppState {
-    dynamodb_client: Client,
-    table_name: String,
+    todo_service: TodoService
 }
 
 #[tokio::main]
@@ -43,8 +45,7 @@ async fn main() -> Result<(), Error> {
     let table_name = &env::var("TABLE_NAME").expect("TABLE_NAME must be set");
 
     let shared_state = Arc::new(AppState {
-        dynamodb_client: dynamodb_client,
-        table_name: table_name.clone(),
+        todo_service: TodoService::new(dynamodb_client, table_name.to_string())
     });
 
     let is_lambda = &env::var("LAMBDA_TASK_ROOT");
@@ -53,8 +54,6 @@ async fn main() -> Result<(), Error> {
         let app = Router::new()
             .route("/", get(root))
             .route("/home", get(home_page).post(home_page_post))
-            .route("/todo", get(list_todo).post(post_todo))
-            .route("/todo/:id", get(get_todo))
             // Add middleware to all layers
             .layer(
                 ServiceBuilder::new()
@@ -79,8 +78,6 @@ async fn main() -> Result<(), Error> {
         let axum_app = Router::new()
             .route("/", get(root))
             .route("/home", get(home_page).post(home_page_post))
-            .route("/todo", get(list_todo).post(post_todo))
-            .route("/todo/:id", get(get_todo))
             // Add middleware to all layers
             .layer(
                 ServiceBuilder::new()
@@ -115,149 +112,22 @@ async fn root() -> Json<Value> {
     Json(json!({ "msg": "Welcome to the Rust ToDo API" }))
 }
 
-async fn list_todo(State(state): State<Arc<AppState>>) -> Json<Value> {
-    let res = state
-        .dynamodb_client
-        .query()
-        .table_name(&state.table_name)
-        .key_condition_expression("PK = :hashKey")
-        .expression_attribute_values(
-            ":hashKey",
-            AttributeValue::S(String::from("USER#JAMESEASTHAM")),
-        )
-        .send()
-        .await;
-
-    let query_result = res.unwrap();
-
-    let mut items: Vec<Todo> = Vec::new();
-
-    query_result
-        .items()
-        .expect("Items to exist")
-        .into_iter()
-        .for_each(|item| {
-            items.push(Todo {
-                id: item["id"].as_s().unwrap().to_string(),
-                text: item["text"].as_s().unwrap().to_string(),
-                completed: *item["completed"].as_bool().unwrap(),
-            });
-        });
-
-    Json(json!(items))
-}
-
-async fn get_todo(Path(id): Path<String>, State(state): State<Arc<AppState>>) -> Json<Value> {
-    let res = state
-        .dynamodb_client
-        .get_item()
-        .table_name(&state.table_name)
-        .key("PK", AttributeValue::S("USER#JAMESEASTHAM".to_string()))
-        .key(
-            "SK",
-            AttributeValue::S(String::from(format!("TODO#{0}", id.to_uppercase()))),
-        )
-        .send()
-        .await;
-
-    let response_value = res.unwrap();
-    let result_item = response_value.item().expect("Item should exist");
-
-    let todo = Todo {
-        id: result_item["id"].as_s().unwrap().to_string(),
-        text: result_item["text"].as_s().unwrap().to_string(),
-        completed: *result_item["completed"].as_bool().unwrap(),
-    };
-
-    Json(json!(todo))
-}
-
 /// Home page handler; just render a template with some arguments.
 async fn home_page(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let res = state
-        .dynamodb_client
-        .query()
-        .table_name(&state.table_name)
-        .key_condition_expression("PK = :hashKey")
-        .expression_attribute_values(
-            ":hashKey",
-            AttributeValue::S(String::from("USER#JAMESEASTHAM")),
-        )
-        .send()
-        .await;
-
-    let query_result = res.unwrap();
-
-    let mut items: Vec<Todo> = Vec::new();
-
-    query_result
-        .items()
-        .expect("Items to exist")
-        .into_iter()
-        .for_each(|item| {
-            items.push(Todo {
-                id: item["id"].as_s().unwrap().to_string(),
-                text: item["text"].as_s().unwrap().to_string(),
-                completed: *item["completed"].as_bool().unwrap(),
-            });
-        });
+    let items = state.todo_service.list_todos().await;
 
     render!(templates::page_html, items)
 }
 
-async fn home_page_post(State(state): State<Arc<AppState>>, form: Form<CreateTodo>) {
+async fn home_page_post(
+    State(state): State<Arc<AppState>>,
+    form: Form<CreateTodo>,
+) -> impl IntoResponse {
     tracing::debug!("Creating {}", form.text.clone());
 
-    let todo = Todo {
-        completed: false,
-        text: form.text.clone(),
-        id: Uuid::new_v4().to_string(),
-    };
+    state.todo_service.create_todo(form.0).await;
 
-    let res = state
-        .dynamodb_client
-        .put_item()
-        .table_name(&state.table_name)
-        .item("PK", AttributeValue::S(String::from("USER#JAMESEASTHAM")))
-        .item(
-            "SK",
-            AttributeValue::S(String::from(format!("TODO#{0}", &todo.id.to_uppercase()))),
-        )
-        .item("text", AttributeValue::S(todo.text.to_string()))
-        .item("id", AttributeValue::S(todo.id.to_string()))
-        .item("completed", AttributeValue::Bool(todo.completed))
-        .send()
-        .await;
-
-    ()
-}
-
-async fn post_todo(
-    State(state): State<Arc<AppState>>,
-    Json(input): Json<CreateTodo>,
-) -> Json<Value> {
-    let todo = Todo {
-        completed: false,
-        text: input.text,
-        id: Uuid::new_v4().to_string(),
-    };
-
-    let res = state
-        .dynamodb_client
-        .put_item()
-        .table_name(&state.table_name)
-        .item("PK", AttributeValue::S(String::from("USER#JAMESEASTHAM")))
-        .item(
-            "SK",
-            AttributeValue::S(String::from(format!("TODO#{0}", &todo.id.to_uppercase()))),
-        )
-        .item("text", AttributeValue::S(todo.text.to_string()))
-        .item("id", AttributeValue::S(todo.id.to_string()))
-        .item("completed", AttributeValue::Bool(todo.completed))
-        .send()
-        .await;
-
-    Json(json!(todo))
+    Redirect::to("/home")
 }
 
 /// This method can be used as a "template tag", i.e. a method that
@@ -270,18 +140,6 @@ fn nav(out: &mut impl Write) -> std::io::Result<()> {
             ("axum", "https://crates.io/crates/axum"),
         ],
     )
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct CreateTodo {
-    text: String,
-}
-
-#[derive(Debug, Serialize, Clone)]
-pub struct Todo {
-    id: String,
-    text: String,
-    completed: bool,
 }
 
 include!(concat!(env!("OUT_DIR"), "/templates.rs"));
---
+++ src/axum-lambda-server-side-rendering/src/services.rs
@@ -0,0 +1,86 @@
+pub mod services {
+    use aws_sdk_dynamodb::{Client, model::AttributeValue};
+    use serde::{Deserialize, Serialize};
+    use uuid::Uuid;
+
+    pub struct TodoService {
+        client: Client,
+        table_name: String,
+    }
+
+    impl TodoService {
+        pub fn new(client: Client, table_name: String) -> TodoService {
+            TodoService {
+                client: client,
+                table_name: table_name,
+            }
+        }
+
+        pub async fn list_todos(&self) -> Vec<Todo> {
+            let res = &self
+                .client
+                .query()
+                .table_name(&self.table_name)
+                .key_condition_expression("PK = :hashKey")
+                .expression_attribute_values(
+                    ":hashKey",
+                    AttributeValue::S(String::from("USER#JAMESEASTHAM")),
+                )
+                .send()
+                .await;
+
+            let query_result = res.as_ref().unwrap();
+
+            let mut items: Vec<Todo> = Vec::new();
+
+            query_result
+                .items()
+                .expect("Items to exist")
+                .into_iter()
+                .for_each(|item| {
+                    items.push(Todo {
+                        id: item["id"].as_s().unwrap().to_string(),
+                        text: item["text"].as_s().unwrap().to_string(),
+                        completed: *item["completed"].as_bool().unwrap(),
+                    });
+                });
+
+            items
+        }
+    
+        pub async fn create_todo(&self, input: CreateTodo) {
+            let todo = Todo {
+                completed: false,
+                text: input.text.clone(),
+                id: Uuid::new_v4().to_string(),
+            };
+        
+            let res = &self
+                .client
+                .put_item()
+                .table_name(&self.table_name)
+                .item("PK", AttributeValue::S(String::from("USER#JAMESEASTHAM")))
+                .item(
+                    "SK",
+                    AttributeValue::S(String::from(format!("TODO#{0}", &todo.id.to_uppercase()))),
+                )
+                .item("text", AttributeValue::S(todo.text.to_string()))
+                .item("id", AttributeValue::S(todo.id.to_string()))
+                .item("completed", AttributeValue::Bool(todo.completed))
+                .send()
+                .await;
+        }
+    }
+
+    #[derive(Debug, Serialize, Clone)]
+    pub struct Todo {
+        pub id: String,
+        pub text: String,
+        pub completed: bool,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct CreateTodo {
+        pub text: String,
+    }
+}
